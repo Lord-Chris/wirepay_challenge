4ee2e18ec27e9ecd0d51474ccbe535d64b7608a9
Lord-Chris <maduekechris65@gmail.com>
2024-07-20 10:25:50 +0100

    CHORE: Add route and route dependencies

---
+++ lib/core/router/router.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'routes.dart';
+
+abstract class AppRouter {
+  static Route<dynamic> generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.passcode:
+        return _getPageRoute(
+          settings: settings,
+          view: const Placeholder(),
+        );
+
+      case Routes.homeView:
+        return _getPageRoute(
+          settings: settings,
+          view: const Placeholder(),
+        );
+
+      default:
+        return _getPageRoute(
+          settings: settings,
+          view: Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+
+  static PageRoute<dynamic> _getPageRoute({
+    required RouteSettings settings,
+    required Widget view,
+  }) {
+    return Platform.isIOS
+        ? CupertinoPageRoute(settings: settings, builder: (_) => view)
+        : MaterialPageRoute(settings: settings, builder: (_) => view);
+  }
+}
